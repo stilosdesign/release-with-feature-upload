31da905b13252afe2b1a576ca1538991191575e2
antonielbordin <antonielbordin@hotmail.com>
2023-07-30 17:59:57 -0300

    Change to lower case variables locales

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,49 +4,49 @@ set -e
 
 echo ''
 
-# env
+# versions
 echo "node version: $(node -v)"
 echo "npm version: $(npm -v)"
 
-# Define variables.
-AUTH="Authorization: token $GITHUB_TOKEN"
-GH_API="https://api.github.com"
-GH_REPO="$GH_API/repos/$GITHUB_USERNAME/$GITHUB_REPO"
-TYPE_RELEASE=$GITHUB_TYPE_RELEASE
-RELEASE_TAG=$GITHUB_RELEASE_TAG
+# Define variables locales.
+auth="Authorization: token $GITHUB_TOKEN"
+gh_api="https://api.github.com"
+gh_repo="$gh_api/repos/$GITHUB_USERNAME/$GITHUB_REPO"
+type_release=$GITHUB_TYPE_RELEASE
+release_tag=$GITHUB_RELEASE_TAG
 
 # Validate is filled tag name
-if [[ "$RELEASE_TAG" == " " ]]; then
+if [[ "$release_tag" == "" ]]; then
   # Mounted URL for get last release tag
-  GH_TAGS="$GH_REPO/releases/latest" 
+  gh_tags="$gh_repo/releases/latest" 
 
   # Get last release tag
-  LAST_RELEASE_TAG=$(curl -sL "${GH_TAGS}" | jq -r ".tag_name")
+  last_release_tag=$(curl -sL "${gh_tags}" | jq -r ".tag_name")
 
   # Validate tag an generate new tag
-  if [[ "$LAST_RELEASE_TAG" == 'null' ]]; then
-    NEXT_RELEASE_TAG="v1.0.0"
+  if [[ "$last_release_tag" == 'null' ]]; then
+    next_release_tag="v1.0.0"
   else
     major=$(awk -F'.' '{print $1}' <<< "$LAST_RELEASE_TAG")
     minor=$(awk -F'.' '{print $2}' <<< "$LAST_RELEASE_TAG")
     patch=$(awk -F'.' '{print $3}' <<< "$LAST_RELEASE_TAG")
 
     # Generate new tag [major, minor, patch]
-    if [[ "$TYPE_RELEASE" == "major" ]]; then
+    if [[ "$type_release" == "major" ]]; then
       cMajor=$(awk -F'v' '{print $2}' <<< "$major")
       major="v"$(($cMajor+1))
     fi
-    if [[ "$TYPE_RELEASE" == "minor" ]]; then
+    if [[ "$type_release" == "minor" ]]; then
       minor=$(($minorr+1))
     fi
-    if [[ "$TYPE_RELEASE" == "patch" ]]; then
+    if [[ "$type_release" == "patch" ]]; then
       patch=$(($patch+1))
     fi  
-    NEXT_RELEASE_TAG+="$major.$minor.$patch"
+    next_release_tag="$major.$minor.$patch"
   fi
 else
-  NEXT_RELEASE_TAG=$RELEASE_TAG
+  next_release_tag=$release_tag
 fi
 
-echo "new tag release: $NEXT_RELEASE_TAG"
+echo "new tag release: $next_release_tag"
 
